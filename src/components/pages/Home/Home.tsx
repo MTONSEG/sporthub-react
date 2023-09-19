ecbf8a6382127bcf62b9bbf91bb65446c9326830
Maksym Puzanov <puzanov.maksym@gmail.com>
2023-09-20 00:51:04 +0300

    update vs code

--- src/components/pages/Home/Home.tsx
+++ src/components/pages/Home/Home.tsx
@@ -2,9 +2,7 @@ import React from "react";
 import './Home.scss';
 import { useAppSelector } from "../../../hooks/hooks";
 import Slider from "../../ui/sliders/Slider/Slider";
-import { Settings } from "react-slick";
 import { SwiperOptions } from "swiper/types/swiper-options";
-import { Title } from "../../ui/atoms/Title/Title";
 import VideoList from "../../common/VideoList/VideoList";
 
 
@@ -28,7 +26,7 @@ const Home: React.FC = () => {
 	}
 
 	return (
-		<div className="home">
+		<div className='home'>
 			<div className="home__slider">
 				<Slider
 					settings={sliderSetting}
